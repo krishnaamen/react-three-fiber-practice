ab3fbea7dd4415a3f960cd7a0c0da4ee41180c5e
Krishna Prasad Khanal <krishnaamen@gmail.com>
2024-03-11 12:24:13 +0100

    adding torus

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,42 @@ const Sphere = ({ position, size, color }) => {
     ref.current.rotation.y += delta
     ref.current.position.x = Math.sin(state.clock.elapsedTime) * 2;
     ref.current.position.y = Math.sin(state.clock.elapsedTime) * 2;
-    
+
 
   })
 
-  return(
+  return (
     <mesh position={position} ref={ref}>
-      <sphereGeometry args={size}/>
-      <meshStandardMaterial  color={color}/>
+      <sphereGeometry args={size} />
+      <meshStandardMaterial color={color} />
     </mesh>
-    
+
   )
 }
+
+
+const Torus = ({ position, size, color }) => {
+  const ref = useRef();
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta
+    ref.current.rotation.y += delta
+    ref.current.position.x = Math.sin(state.clock.elapsedTime);
+    ref.current.position.z = Math.sin(state.clock.elapsedTime);
+
+
+  })
+
+  return (
+    <mesh position={position} ref={ref}>
+      <torusGeometry args={size} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+
+  )
+}
+
+
+
 
 const Cube = ({ position, size, color }) => {
   const ref = useRef()
@@ -30,9 +54,6 @@ const Cube = ({ position, size, color }) => {
     ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
 
   })
-
-
-  
 
   return (
     <mesh position={position} ref={ref}>
@@ -53,7 +74,7 @@ function App() {
     <>
       <h1>Cube</h1>
       <Canvas>
-      
+
         <directionalLight position={[0, 0, 2]} />
         <Cube position={[0, 0, 2]} size={[2, 2, 2]} color={'orange'} />
 
@@ -68,10 +89,16 @@ function App() {
 
 
       </Canvas>
-        <h1>Sphere</h1>
+      <h1>Sphere</h1>
       <Canvas>
-      <directionalLight position={[2, 1, 10]} />
-        <Sphere  position={[1,0,0]} size={[1.5,30,30]} color={'red'}/>
+        <directionalLight position={[2, 1, 10]} />
+        <Sphere position={[1, 0, 0]} size={[1.5, 30, 30]} color={'red'} />
+      </Canvas>
+
+      <h1>Torus</h1>
+      <Canvas>
+        <directionalLight position={[2, 1, 10]} />
+        <Torus position={[1, 0, 0]} size={[1,0.1, 30, 30]} color={'blue'} />
       </Canvas>
 
     </>
